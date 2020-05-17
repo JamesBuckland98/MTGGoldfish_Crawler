538fa36f3a6d7b2ebe22b23add4c3e6ace48c9ec
James Buckland <jamesbuckland98@gmai.com>
2020-05-17 18:41:51 +0100

    Added optional arguments

--- src/automatedWebBrowsing/AutoBrowsing.java
+++ src/automatedWebBrowsing/AutoBrowsing.java
@@ -5,6 +5,13 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.openqa.selenium.WebDriver;
 
 import helper.Connection;
@@ -17,10 +24,33 @@ public class AutoBrowsing {
 	private static final Logger LOGGER = Logger.getLogger(AutoBrowsing.class.getName());
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		WebDriver driver = null;
+		
+		Options options = new Options();
+		Option headlessMode = new Option("b", false, "Run in background mode");
+		options.addOption(headlessMode);
+
 		List<String> cardList = Read.readTxtFile(args[0]);
 		List<String> blacklist = Read.readTxtFile("blacklist.txt");
 		
-		WebDriver driver = Config.setUp();
+		CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        
+        try {
+        	CommandLine cmd = parser.parse(options, args);
+        	if(cmd.hasOption("b")) {
+        		driver = Config.setUp("headless");
+        	} else {
+        		driver = Config.setUp();
+        	}
+        } catch (ParseException e) {
+            LOGGER.log(Level.WARNING, e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
+		
+		
 		
 		if(Connection.checkInternetConnection()) {
 			for(String cardName: cardList) {
--- src/setUp/Config.java
+++ src/setUp/Config.java
@@ -7,15 +7,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Config {
-	public static WebDriver setUp() {
+	public static WebDriver setUp(String headlessMode) {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		System.setProperty("webdriver.chrome.silentOutput", "true");
 		
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
+		options.addArguments(headlessMode);
 
 		//Initiating  chromedriver
 		WebDriver driver= new ChromeDriver(options);
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
+		return driver;		
+	}
+	
+	public static WebDriver setUp() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+
+		//Initiating  chromedriver
+		WebDriver driver= new ChromeDriver();
 		
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
